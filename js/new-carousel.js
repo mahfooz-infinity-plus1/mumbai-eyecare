d7f90e7329ffa0a574f65ac4f30f22b680987d17
suprit-infinity-plus1 <suprit@infinityplus1.in>
2026-04-07 16:41:40 +0530

    doctor carousal code added

--- js/new-carousel.js
+++ js/new-carousel.js
@@ -14,25 +14,49 @@ $(document).ready(function () {
     });
 
 
-    $(".doctor-carousel").owlCarousel({
+    // $(".doctor-carousel").owlCarousel({
+    //     loop: true,
+    //     margin: 30,
+    //     autoplay: true,
+    //     autoplayTimeout: 0,        // Continuous movement
+    //     autoplaySpeed: 8000,       // Adjust for flow speed
+    //     smartSpeed: 8000,          // Match for constant speed
+    //     slideTransition: 'linear', // Required for smooth continuous movement
+    //     autoplayHoverPause: false, // Prevents jerky stops on hover
+    //     dots: false,
+    //     nav: false,
+    //     responsive: {
+    //         0: { items: 1 },
+    //         600: { items: 2 },
+    //         1000: { items: 3 }
+    //     },
+    // });
+
+    // // Start immediately
+    // $(".doctor-carousel").trigger('play.owl.autoplay', [1]);
+
+    $('.doctor-carousel').owlCarousel({
         loop: true,
-        margin: 30,
+        items: 3,
+        margin: 10,
         autoplay: true,
-        autoplayTimeout: 0,        // Continuous movement
-        autoplaySpeed: 8000,       // Adjust for flow speed
-        smartSpeed: 8000,          // Match for constant speed
-        slideTransition: 'linear', // Required for smooth continuous movement
-        autoplayHoverPause: false, // Prevents jerky stops on hover
+        autoplayTimeout: 3000,
+        autoplaySpeed: 3000,
+        smartSpeed: 3000,
         dots: false,
         nav: false,
+        autoplayHoverPause: true,
+        responsiveClass: true,
         responsive: {
-            0: { items: 1 },
-            600: { items: 2 },
-            1000: { items: 3 }
-        },
-    });
-
-    // Start immediately
-    $(".doctor-carousel").trigger('play.owl.autoplay', [1]);
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+        }
+    })
 
 });
